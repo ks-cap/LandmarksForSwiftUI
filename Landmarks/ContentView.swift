c5ae90e0cec534711247ee18c22269447aa55967
ks-cap <capricorn090114@gmail.com>
2019-09-16 09:35:27 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 佐藤 賢 on 2019/09/16.
+//  Copyright © 2019 佐藤 賢. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
